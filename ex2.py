94675f3ede7fc1c87d16bb9a8231ca421991adcf
cesgah <cgarciahuesca@gmail.com>
2022-09-29 19:32:41 +0000

    Código primos

--- ex2.py
+++ ex2.py
@@ -1,27 +1,37 @@
-#Ejercicio de arreglo de 1 hasta n números, se identificarán qué números son primos
+"""Ejercicio de arreglo de 1 hasta n números, se identificarán qué números son primos
+Se importa la librería sys para correr el programa desde la consola, indicando el input deseado"""
 import sys
+""" 
+Función que crea una lista comenzando del número 1 hasta el número ingresado, 
+esto se hace con un contador y regresa la lista
+"""
 def array (n):
     arr=[]
     i=1
     while i<=n:
         arr.append(i)
-        i+=
-        #comentario
+        i+=1
     return arr
+""" 
+Función que toma la lista pasada para crear una nueva con solo los número primos
+existentes. Esto se hace con la operación de residuos para así identificar y 
+añadir únicamente los números primos, cuando únicamente es divisible entre uno y
+si mismo.
+"""
 def primos (arr):
-    primal=[]
-    y=1
-    for x in arr:
-        if x/y==1:
-            y+=1
-            primal.append(y)
-            for z in primal:
-                if z!=1 && z!=y && kind(y/z)==int:
-                    primal.delete(z)
-    return primal
-n=sys.argv[1]
-n=int(n)
+    primo=[]
+    for y in arr:
+        z=0
+        for x in arr:
+            if y%x==0:
+                z+=1
+        if z==2 or y==1:
+            primo.append(y)
+    print(primo)
+"""
+Se introduce el argumento y se corren las funciones
+"""
+n=int(sys.argv[1])
 arr=array(n)
-print(arr)
-print(primos(arr))
+primos(arr)
 
